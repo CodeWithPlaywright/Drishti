becda3009e47ff91d4dd93b254f3f7e88376d684
IfocusVijayalaxmi <vijayalaxmi.b@ifocussystec.in>
2025-01-22 16:52:22 +0530

    HomePageChange

---
+++ Pages/HomePage.js
@@ -0,0 +1,33 @@
+exports.HomePage = class HomePage {
+
+    constructor(page) {
+        this.page = page
+        this.ProfileIcon = page.locator("//button[@aria-haspopup='menu']")
+        this.logout = page.locator("//div[text()='Logout']")
+        this.MyData= page.locator("//h3[text()='My Data']")
+        this.MyIssue = page.locator("//h3[text()='My Issues']")
+        this.DrishtiText=page.locator("//div[text()='Drishti']")
+        this.ResetPassword = page.locator("//div[text()='Reset Password']")
+    }
+
+    async ClickOnMyIsse() 
+    {
+             await this.MyIssue.click()
+    }
+    async ClickOnMyData()
+    {
+        await this.MyData.click()
+    }
+
+    async ClickOnProfileIconForLogout()
+    {
+        await this.ProfileIcon.click()
+        await this.logout.click()
+        
+    }
+    async ResetPasswordAction()
+    {
+        await this.ResetPassword.click()
+    }
+
+}
